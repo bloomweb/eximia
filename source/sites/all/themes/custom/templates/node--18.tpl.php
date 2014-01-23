addb84629057bba12051f7d0a482178341ca137a
Ricardo Pandales <ricardopandales@gmail.com>
2014-01-23 09:17:19 -0500

    faq mobile

--- source/sites/all/themes/custom/templates/node--18.tpl.php
+++ source/sites/all/themes/custom/templates/node--18.tpl.php
@@ -10,34 +10,43 @@
 
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-	<?php if($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
-		<header>
-			<?php print render($title_prefix); ?>
-			<?php if(!$page && $title): ?>
-				<h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-			<?php endif; ?>
-			<?php print render($title_suffix); ?>
 
-			<?php if($display_submitted): ?>
-				<p class="submitted">
-					<?php print $user_picture; ?>
-					<?php print $submitted; ?>
-				</p>
-			<?php endif; ?>
-
-			<?php if($unpublished): ?>
-				<mark class="unpublished"><?php print t('Unpublished'); ?></mark>
-			<?php endif; ?>
-		</header>
-	<?php endif; ?>
 	
 	<div class="wrapper">
-		<?php
+		
+		
+	<div class="body-content">
+        <?php if($title_prefix || $title_suffix || $display_submitted || $unpublished || $title): ?>
+
+            <?php print render($title_prefix); ?>
+            <?php if($title): ?>
+                <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+
+            <?php if($display_submitted): ?>
+                <p class="submitted">
+                    <?php print $user_picture; ?>
+                    <?php print $submitted; ?>
+                </p>
+            <?php endif; ?>
+
+            <?php if($unpublished): ?>
+                <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
+            <?php endif; ?>
+
+        <?php endif; ?>
+
+        <?php
+
 			// We hide the comments and links now so that we can render them later.
 			hide($content['comments']);
 			hide($content['links']);
+
 			print render($content);
 		?>
+	</div>
+
 		<div class="faq-block-wrapper">
 			<?php print views_embed_view('faqs', 'block_faqs'); ?>
 		</div>
