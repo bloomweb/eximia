f060a52695554564b13dc45929f758efff24899c
Julio César Domínguez Giraldo <juliodominguez@bloomweb.co>
2014-03-06 06:05:32 -0500

    revs

--- source/sites/all/themes/custom/templates/page--node--how-can-we-help.tpl.php
+++ source/sites/all/themes/custom/templates/page--node--how-can-we-help.tpl.php
@@ -57,11 +57,16 @@
             <ul class="action-links"><?php print render($action_links); ?></ul>
         <?php endif; ?>
 		<div class="left">
+			<?php if($node->nid == 65) : ?>
+				<p>
+					<?php echo strtoupper($node->title); ?>
+				</p>
+			<?php endif; ?>
 			<?php
 				if(isset($node->field_column_a['und'][0]['value'])) {
 					print $node->field_column_a['und'][0]['value'];
 				}
-				if(isset($node->field_column_b['und'][0]['value'])) {
+				if(isset($node->field_column_b['und'][0]['value']) && $node->nid != 64) {
 					print '<a href="#" class="more-intro">+</a>';
 				}
 			?>
@@ -75,7 +80,7 @@
 		</div>
 	</div>
 
-	<?php if($node->title != 'Training and consulting') : ?>
+	<?php if($node->nid != 65) : ?>
 	<div id="main">
 
 		<div id="content" class="column" role="main">
